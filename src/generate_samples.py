3a88ffec965e730550bd23dabf6d10ded542707a
florian <florian.krgl@gmail.com>
2023-07-19 14:34:05 +0200

    plot generated samples

--- src/generate_samples.py
+++ src/generate_samples.py
@@ -32,22 +32,23 @@ def get_gene_encode_decode(token_names: list):
                                  index_list]  # decoder: take a list of integers, output a string
     return encode, decode
 
+
 # load models
 model = TransformerModel(d_model=d_model,
-                                     dim_feedforward=dim_feedforward,
-                                     nlayers=nlayers,
-                                     n_input_bins=n_input_bins,
-                                     n_token=n_token,
-                                     nhead=nhead)
+                         dim_feedforward=dim_feedforward,
+                         nlayers=nlayers,
+                         n_input_bins=n_input_bins,
+                         n_token=n_token,
+                         nhead=nhead)
 model.load_state_dict(torch.load('../data/model_1.pth', map_location=torch.device('cpu')))
 model.eval()
 
 model2 = TransformerModel(d_model=d_model,
-                                     dim_feedforward=dim_feedforward,
-                                     nlayers=nlayers,
-                                     n_input_bins=n_input_bins,
-                                     n_token=n_token,
-                                     nhead=nhead)
+                          dim_feedforward=dim_feedforward,
+                          nlayers=nlayers,
+                          n_input_bins=n_input_bins,
+                          n_token=n_token,
+                          nhead=nhead)
 model2.load_state_dict(torch.load('../data/model_2.pth', map_location=torch.device('cpu')))
 model2.eval()
 
@@ -77,28 +78,54 @@ print(sample_profile)
 sample_profile = model2.generate(x_src, val, np.arange(100))
 bin_to_expression = p.bin_to_expression
 to_expression = lambda bins: [bin_to_expression[b] for b in bins]
+data_preprocessed = np.empty_like(data[0])
+for binned_data in data:
+    data_preprocessed = np.vstack([data_preprocessed, to_expression(binned_data)])
+print(data_preprocessed.shape)
+
 # sample multiple profiles
-n_samples = 1000
+n_samples = 10000
 expression_profiles = np.array(to_expression(val.detach().numpy()))
 for i in range(n_samples):
     sample_profile = model2.generate(x_src, val, np.arange(100))
     expression = to_expression(sample_profile)
     expression_profiles = np.vstack((expression_profiles, expression))
-print(expression_profiles)
-std = expression_profiles.std(axis=0)
-print(f'std for each gene: {expression_profiles.std(axis=0)}')
+# print(expression_profiles)
+# std = expression_profiles.std(axis=0)
+# print(f'std for each gene: {expression_profiles.std(axis=0)}')
+#
+#  # umap
+# umap_2d = UMAP(n_components=2, init='random', random_state=0)
+# expression_profiles = np.round(expression_profiles, decimals=5)
+# proj_2d = umap_2d.fit_transform(expression_profiles)
+# print(proj_2d)
+# color = ['blue'] * expression_profiles.shape[0]
+# color[0] = 'orange'
+# plt.scatter(proj_2d[1:,0], proj_2d[1:,1], c='blue', alpha=.5)
+# plt.scatter(proj_2d[0,0], proj_2d[0,1], c='orange', s=100)
+# plt.title(f'umap of {n_samples} generated expression profiles')
+# plt.xlabel('umap 1')
+# plt.ylabel('umap 2')
+# #plt.show()
+# plt.savefig('Umap_generated_amples.png')
 
- # umap
+# create umap
 umap_2d = UMAP(n_components=2, init='random', random_state=0)
-expression_profiles = np.round(expression_profiles, decimals=5)
-proj_2d = umap_2d.fit_transform(expression_profiles)
+data_preprocessed = np.round(data_preprocessed, decimals=5)
+proj_2d = umap_2d.fit_transform(np.vstack([data_preprocessed, expression_profiles]))
+src_profile = np.array(to_expression(val.detach().numpy()))
+print(src_profile)
+proj_src = umap_2d.transform(src_profile.reshape(1, -1))
+#proj_generated_profiles = umap_2d.transform(expression_profiles)
 print(proj_2d)
-color = ['blue'] * expression_profiles.shape[0]
-color[0] = 'orange'
-plt.scatter(proj_2d[1:,0], proj_2d[1:,1], c='blue', alpha=.5)
-plt.scatter(proj_2d[0,0], proj_2d[0,1], c='orange', s=100)
-plt.title(f'umap of {n_samples} generated expression profiles')
-plt.xlabel('umap 1')
-plt.ylabel('umap 2')
-#plt.show()
-plt.savefig('Umap_generated_amples.png')
+print(proj_src[0])
+n_pancreas = data_preprocessed.shape[0]
+fig, ax = plt.subplots(1,2, figsize=(10,4), sharex=True, sharey=True)
+ax[0].scatter(proj_2d[:n_pancreas, 0], proj_2d[:n_pancreas, 1], c='blue', alpha=.5)
+ax[0].scatter(proj_2d[n_pancreas:, 0], proj_2d[n_pancreas:, 1], c='yellow', alpha=0.5)
+ax[0].scatter(proj_src[0][0], proj_src[0][1], c='green', s=100)
+fig.suptitle(f'umap of binned expression profiles')
+fig.supxlabel('umap 1')
+fig.supylabel('umap 2')
+ax[1].scatter(proj_2d[:, 0], proj_2d[:, 1], c='blue', alpha=.5)
+plt.show()
