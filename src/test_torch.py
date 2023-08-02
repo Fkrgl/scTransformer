5d1c171141ced7009d394fd5f22d191ea72e5cf1
florian <florian.krgl@gmail.com>
2023-08-02 08:31:38 +0200

    add function that sets all masked positions to zero

--- src/test_torch.py
+++ src/test_torch.py
@@ -5,15 +5,16 @@ import matplotlib.pyplot as plt
 
 
 a = np.array([[1,2,3], [4,5,6], [7,8,9]])
-b = a
-b[1,1] = 0
-print(a)
-print(b)
+
+print(f'a normal:\n{a}')
+
 mask = np.array([[True, False, True], [True, False, True], [True, False, True]])
-print(a)
-print(a[mask])
+print(f'mask:\n{mask}')
 
 random = np.random.choice(np.arange(100), size=(3,3))
-print(random)
+print(f'random:\n{random}')
 a[mask] = random[mask]
-print(a)
+print(f'a modified:\n{a}')
+
+a[mask] = 0
+print(f'a zero:\n{a}')
